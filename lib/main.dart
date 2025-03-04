20213d7a34c52f9b576f68c9c1a273fa43036b2b
l4limbo <anestis2799@gmail.com>
2025-03-04 19:41:51 +0200

    feat: setup main material app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pokemon_explorer/config/routes/router.dart';
+import 'package:pokemon_explorer/config/theme/app_theme.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(
     ProviderScope(
       child: MyApp(),
@@ -14,13 +18,13 @@ class MyApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Pokemon Explorer')),
-        body: Center(
-          child: Text('Pokemon'),
-        ),
-      ),
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      routeInformationParser: router.routeInformationParser,
+      routerDelegate: router.routerDelegate,
+      routeInformationProvider: router.routeInformationProvider,
+      title: 'Pokemon Explorer',
+      theme: AppTheme.lightTheme(),
     );
   }
 }
