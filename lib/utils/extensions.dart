dc8b36bc7de6147d698f396184b0dcd479a519b2
l4limbo <anestis2799@gmail.com>
2025-03-09 16:45:00 +0200

    fix: formatName extension for two-letter words

--- lib/utils/extensions.dart
+++ lib/utils/extensions.dart
@@ -1,5 +1,9 @@
 extension StringExtensions on String {
   String formatName() {
+    if (length == 2) {
+      return toUpperCase();
+    }
+
     return split('-')
         .map((word) =>
             word.isNotEmpty ? word[0].toUpperCase() + word.substring(1) : '')
